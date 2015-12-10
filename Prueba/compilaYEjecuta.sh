4e7dca48dbb1ca9d72385a9adeb5a14fef7cb1da
hectorgoan <hectorgoan@usal.es>
2015-12-10 14:22:09 +0100

    Copyrights added, Faltan apellidos y dni de nestor

--- Prueba/compilaYEjecuta.sh
+++ Prueba/compilaYEjecuta.sh
@@ -1,7 +1,17 @@
 #! /bin/sh
+
+#####################################
+# Fichero: compilaYEjecuta.sh
+# Autores: 
+# Héctor Gonzalo Andrés DNI 71038384D
+# Néstor 
+#####################################
+
 make
-#lanza dos nuevas ventanas de terminal cada una con un ejecutable
-#para terminar, hacer ctrl+c en la ventana de terminal principal
+
+#Lanza una nueva ventana en la que se ejecuta el cliente
+#el servidor se sigue ejecutando en la ventana de terminal principal
+#para terminar, hacer ctrl+c en la ventana de terminal del servidor
 
 zenity --info --text="Se han lanzado 1 ventana!\nEn la terminal original se ejecuta el servidor.\nEn la nueva se ejecuta el cliente\nPulse crtl+c en el servidor para salir" --title="Atención!" &
 ./servidor & xterm -hold -e ./cliente 127.0.0.1
